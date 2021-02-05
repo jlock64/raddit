7cb2597b77339c8072df216761af0d87b2cc0b2e
Justin Lock <jlock@TRHC.com>
2021-02-05 12:09:08 -0500

    add name to users table and update devise to permit new field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210205160651) do
+ActiveRecord::Schema.define(version: 20210205163759) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20210205160651) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
